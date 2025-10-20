492a61669bbbf3823205d1f8d7da74dda2f58727
Adrian-rospx <adrianrosu5400@gmail.com>
2025-10-20 18:31:02 +0300

    working prototype

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Shooter/Shooter.java
@@ -10,16 +10,21 @@ import org.firstinspires.ftc.teamcode.util.StateMachine;
 
 public class Shooter {
     private DcMotor flywheel;
+    private DcMotor intakeMotor;
     private Servo flap;
     private Servo pivot;
 
+    private final double intakePower = 0.7;
+    private final double flywheelPower = 0.7;
     private final double flapDown = 0.25;
     private final double flapUp = 0.5;
-    private final double pivotDown = 0;
-    private final double pivotUp = 1;
+    private final double pivotDown = 0.1;
+    private final double pivotIdle = 0.32;
+    private final double pivotUp = 0.4;
 
     private enum State{
         IDLE,
+        SHOOTING,
         RUNNING,
         FLAP_UP,
         INTAKE
@@ -28,16 +33,18 @@ public class Shooter {
     public enum Command{
         TOGGLE_SHOOTER,
         ACTIVATE_FLAP,
-        TOGGLE_INTAKE
+        TOGGLE_INTAKE,
+        TOGGLE_IDLE
     }
 
     private StateMachine<State> fsm = new StateMachine<>(State.IDLE);
     private Command unexecutedCommand;
 
     public Shooter(HardwareMap hardwareMap){
-        flywheel = hardwareMap.get(DcMotor.class, "Flywheel");
-        flap = hardwareMap.get(Servo.class, "Flap");
-        pivot = hardwareMap.get(Servo.class, "Pivot");
+        flywheel = hardwareMap.get(DcMotor.class, "flywheel");
+        flap = hardwareMap.get(Servo.class, "flap");
+        pivot = hardwareMap.get(Servo.class, "pivot");
+        intakeMotor = hardwareMap.get(DcMotor.class, "intake");
 
         flap.setDirection(Servo.Direction.REVERSE);
         flap.setPosition(flapDown);
@@ -49,28 +56,49 @@ public class Shooter {
     }
 
     public void setupShooter(){
-        // when IDLE, stop the flywheel
-        fsm.onStateEnter(State.IDLE,  () -> {
-            flywheel.setPower(0);
+
+        fsm.onStateEnter(State.IDLE, () -> {
+            intakeMotor.setPower(0);
+            pivot.setPosition(pivotIdle);
         });
-        fsm.onStateUpdate(State.IDLE,  () -> {
-            // from IDLE, listen for TOGGLE_SHOOTER
-            if(unexecutedCommand == Command.TOGGLE_SHOOTER){
+        fsm.onStateUpdate(State.IDLE, () -> {
+            if (unexecutedCommand == Command.TOGGLE_INTAKE) {
+                unexecutedCommand = null;
+                return State.INTAKE;
+            }
+            if (unexecutedCommand == Command.TOGGLE_IDLE) {
+                unexecutedCommand = null;
+                return State.SHOOTING;
+            }
+            return null;
+        });
+
+        fsm.onStateEnter(State.SHOOTING,  () -> {
+            flywheel.setPower(0);
+            pivot.setPosition(pivotUp);
+        });
+        fsm.onStateUpdate(State.SHOOTING,  () -> {
+            // from IDLE, listen for TOGGLE_SHOOTER, TOGGLE_INTAKE
+            if(unexecutedCommand == Command.TOGGLE_SHOOTER) {
                 unexecutedCommand = null;
                 return State.RUNNING;
+            }
+            if (unexecutedCommand == Command.TOGGLE_IDLE) {
+                unexecutedCommand = null;
+                return State.IDLE;
             }
             return null;
         });
 
         // when RUNNING, power the flywheel
         fsm.onStateEnter(State.RUNNING, () -> {
-            flywheel.setPower(0.7);
+            flywheel.setPower(flywheelPower);
         });
         fsm.onStateUpdate(State.RUNNING, () -> {
             // from RUNNING, listen for TOGGLE_SHOOTER and ACTIVATE_FLAP
             if(unexecutedCommand == Command.TOGGLE_SHOOTER){
                 unexecutedCommand = null;
-                return State.IDLE;
+                return State.SHOOTING;
             }
             if(unexecutedCommand == Command.ACTIVATE_FLAP){
                 unexecutedCommand = null;
@@ -95,12 +123,31 @@ public class Shooter {
             flap.setPosition(flapDown);
         });
 
+        // when INTAKE, start intakeMotor and set pivot down
+        fsm.onStateEnter(State.INTAKE, () -> {
+            intakeMotor.setPower(intakePower);
+            pivot.setPosition(pivotDown);
+        });
+        fsm.onStateUpdate(State.INTAKE, () -> {
+            // from INTAKE, wait for the TOGGLE_INTAKE
+            if (unexecutedCommand == Command.TOGGLE_INTAKE) {
+                unexecutedCommand = null;
+                return State.IDLE;
+            }
+            return null;
+        });
+        // when leaving INTAKE reset the the pivot servo
+        fsm.onStateExit(State.INTAKE, () -> {
+            pivot.setPosition(pivotUp);
+        });
+
         // final initialisation
         fsm.init();
     }
 
     public void updateShooter(){
         fsm.update();
-        Log.d("stateFlywheel", "" + fsm.getCurrentState());
+        String str = "" + fsm.getCurrentState();
+        Log.d("stateFlywheel", str);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TeleOp.java
@@ -14,14 +14,17 @@ public class TeleOp extends OpMode {
 
      private EdgeDetector toggleShooter = new EdgeDetector(false);
      private EdgeDetector launchGE = new EdgeDetector(false);
-
+     private EdgeDetector toggleIntake = new EdgeDetector(false);
+    private EdgeDetector toggleIdle = new EdgeDetector(false);
     @Override
     public void init() {
         movement = new Movement(hardwareMap);
         shooter = new Shooter(hardwareMap);
         // shooter command setup
-        toggleShooter.onPress(() -> shooter.command(Shooter.Command.TOGGLE_SHOOTER) );
+        toggleShooter.onPress(() -> shooter.command(Shooter.Command.TOGGLE_SHOOTER));
         launchGE.onPress(() -> shooter.command(Shooter.Command.ACTIVATE_FLAP));
+        toggleIntake.onPress(() -> shooter.command(Shooter.Command.TOGGLE_INTAKE));
+        toggleIdle.onPress(() -> shooter.command(Shooter.Command.TOGGLE_IDLE));
 
         shooter.setupShooter();
     }
@@ -37,6 +40,8 @@ public class TeleOp extends OpMode {
         // shooter controls
         toggleShooter.update(gamepad1.cross);
         launchGE.update(gamepad1.triangle);
+        toggleIntake.update(gamepad1.square);
+        toggleIdle.update(gamepad1.left_bumper);
 
         shooter.updateShooter();
     }
