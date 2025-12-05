f0885705d52326d6553c36b2ce2aa02e1ca8505c
Adrian-rospx <adrianrosu5400@gmail.com>
2025-12-05 16:41:53 +0200

    slight tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoPoses.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/AutoPoses.java
@@ -5,18 +5,18 @@ import com.pedropathing.geometry.Pose;
 public class AutoPoses {
     private static final double
             // goal start specifics
-            GOAL_START_X = 22, GOAL_START_Y = 144-22, GOAL_START_ANGLE = -45,
-            GOAL_HOME_X = 24, GOAL_HOME_Y = 90, GOAL_HOME_ANGLE = 180,
+            GOAL_START_X = 22.6, GOAL_START_Y = 144-22.6, GOAL_START_ANGLE = -45,
+            GOAL_HOME_X = 24, GOAL_HOME_Y = 144-54, GOAL_HOME_ANGLE = 180,
             // park start specifics
             PARK_START_X = 0, PARK_START_Y = 0, PARK_START_ANGLE = 0,
             PARK_HOME_X = 24, PARK_HOME_Y = 0, PARK_HOME_ANGLE = 0,
             // shoot
-            SHOOT_X = 55.4, SHOOT_Y = 144 - 55.4, SHOOT_ANGLE = -45,
+            SHOOT_X = 56, SHOOT_Y = 144-56, SHOOT_ANGLE = -45,
             // intake positions
             INTAKE_START_X = 45, INTAKE_ANGLE = 180,
-            FIRST_INTAKE_END_X = 16, FIRST_INTAKE_Y = 84,
-            SECOND_INTAKE_END_X = 8, SECOND_INTAKE_Y = 60,
-            THIRD_INTAKE_END_X = 8, THIRD_INTAKE_Y = 36;
+            FIRST_INTAKE_END_X = 15, FIRST_INTAKE_Y = 144-63,
+            SECOND_INTAKE_END_X = 9, SECOND_INTAKE_Y = FIRST_INTAKE_Y - 24,
+            THIRD_INTAKE_END_X = 9, THIRD_INTAKE_Y = FIRST_INTAKE_Y - 48;
 
     public final Pose
             shoot,
